0b6d745145e7818f576e93b465d5e2e95fa15516
OnateK <ketano1711@gmail.com>
2018-09-17 14:49:20 +0300

    no event added

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,16 +3,64 @@ import Header from "./header/header";
 import Footer from "./header/footer.js";
 import Card from "./events/card.jsx";
 import ContMenu from "./events/contextualmenu.jsx";
+import NoEvents from "./events/noEvents.jsx"
 export default class User extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            toggleArrow:true,
+            isLoading: true,
+            contacts: []
+        }
+        this.callbackHandleArrow = this.callbackHandleArrow.bind(this);
+    }
+
+    callbackHandleArrow(dataFromChildren){
+        this.setState({
+            toggleArrow:dataFromChildren
+        })
+    }
+    componentDidMount(){
+        this.fetchData();
+    }
+
+    fetchData(){
+
+        fetch('https://localhost:8080/api/event')
+        .then(response =>response.json())
+        .then(parsedJSON => parsedJSON.map(user =>({
+           
+            name: `${user.schemaId}`,
+            username: `${user.startTime}`,
+            location: `${user.location}`
+        })))
+        .then(contacts => this.setState({
+            contacts,
+            isLoading: true
+        }))
+        .catch(error => console.log('parsing failed', error))
+
+
+    }
+
     render(){
+        const {isLoading,contacts} = this.state;
         return(
             <div className = "events__div"> 
                 <Header name="Dashboard" />
                 <div className="main__card__component">
-                <Card />
+                {
+                     isLoading && contacts.length > 0 ? contacts.map(contact =>{
+                         return <Card callbackFromParent={this.callbackHandleArrow}/>
+                         
+                     }) : <NoEvents />
+                }
                 </div>
-                <ContMenu />
-                <Footer />
+                <ContMenu 
+                    toggleArrow={ !this.state.toggleArrow }
+                />
+                <Footer className={this.state.toggleArrow ? "" : "arrowOpacity"} />
             </div>
 
 
