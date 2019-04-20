f48f30a030ec8ccaa69ed378327f8d2b91a8d49e
ionut270 <oanceaionut15@gmail.com>
2019-04-20 16:53:16 +0300

    Fixed bug where app craches when you go to home page from add page

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,14 +1,14 @@
-const router = require('express').Router()
-    , express = require('express')
-    , passport = require('passport')
-    , app = express();
+const router = require('express').Router(),
+    express = require('express'),
+    passport = require('passport'),
+    app = express();
 
 //auth logout
 router.get('/logout', (req, res) => {
     req.logout();
-    // res.redirect('/')
-    //handle with pasaport
     res.send('logging out');
+    res.redirect('/')
+    //handle with pasaport
 })
 
 //auth with google
@@ -19,37 +19,26 @@ router.get('/google', passport.authenticate('google', {
 //callback for google to redirect to
 router.get('/google/redirect', passport.authenticate('google',
 
-    { 
+    {
         successRedirect: '/user',
-        failureRedirect: '/' 
+        failureRedirect: '/'
     }
     // res.redirect('/profile');
 ));
 
-
-    // res.redirect('/profile');
-
-
-// router.get('/google/redirect', 
-// 	  passport.authenticate('google', { successRedirect: '/profile',
-// 	                                      failureRedirect: '/' }))
-
 //auth fb
-router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+router.get('/facebook', passport.authenticate('facebook', {
+    scope: ['email']
+}));
 router.get('/facebook/callback', passport.authenticate('facebook',
     // res.send({
     //     credentials: req.user,
     //     logged: true,
     // });
-    { successRedirect: '/user',
-    failureRedirect: '/' 
-}))
-/*router.get('/facebook/callback', passport.authenticate('facebook',{
-        successRedirect: '/profile.html',
+    {
+        successRedirect: '/user',
         failureRedirect: '/'
-    })
-
-);*/
+    }))
 
 router.get('/logged', (req, res) => {
     res.send({
--- src/components/events/addEvent.jsx
+++ src/components/events/addEvent.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import "../../css/index.css";
 import Search from "../events/search.js";
-import axios from 'axios';
+import axios from "axios";
 
-import placeholder from "../../view/images/placeholder.png"
+import placeholder from "../../view/images/placeholder.png";
 import searchEvent from "../../view/images/searchicon.png";
 import "../../sass/main/events/user.scss";
 import Timeset from "./timeset.jsx";
-
 
 export default class addEvent extends React.Component {
     constructor(props) {
@@ -23,31 +22,34 @@ export default class addEvent extends React.Component {
     }
     handleTimer() {
         this.setState({
-            clickTimerButton: !this.state.clickTimerButton,
-            
-
-        })
-      
+            clickTimerButton: !this.state.clickTimerButton
+        });
     }
-    setVenue(res){
+    setVenue(res) {
         this.setState({
             venue: res
-        })
+        });
     }
     componentDidMount() {
         this.getLocation();
     }
     getLocation = () => {
         navigator.geolocation.getCurrentPosition(response => {
-            this.setState({
-                latlong: response.coords.latitude + "," + response.coords.longitude
-            }, () => {
-                this.getVenues("")
-            });
+            this.setState(
+                {
+                    latlong:
+                        response.coords.latitude +
+                        "," +
+                        response.coords.longitude
+                },
+                () => {
+                    this.getVenues("");
+                }
+            );
         });
     };
 
-    getVenues = (query) => {
+    getVenues = query => {
         const endPoint = "https://api.foursquare.com/v2/venues/explore?";
         const params = {
             client_id: "EFSQIR4NPI5MRIF0DMGMF14COGKRFR4FWD3EXKXFTVP0TAKY",
@@ -56,28 +58,24 @@ export default class addEvent extends React.Component {
             query: query,
             limit: 100,
             v: "20181709"
-
         };
         axios.get(endPoint + new URLSearchParams(params)).then(res => {
-
             let restaurant = Object.assign([]);
             let stateBackup = Object.assign([], this.state);
             res.data.response.groups[0].items.map(element => {
-                console.log(element.venue.categories[0].id);
-                if (element.venue.categories[0].id.includes("4bf58dd8d48988d1")) {
-
-
+                //console.log(element.venue.categories[0].id);
+                if (
+                    element.venue.categories[0].id.includes("4bf58dd8d48988d1")
+                ) {
                     // console.log(element.venue.categories[0].id );
                     restaurant.push(element);
                 }
-            })
-            console.log(restaurant);
+            });
+            //console.log(restaurant);
             stateBackup.venues = restaurant;
             this.setState(stateBackup);
-
         });
-
-    }
+    };
     render() {
         return (
             <div className="addEventContainer">
@@ -88,25 +86,43 @@ export default class addEvent extends React.Component {
                             divdisplay={this.state.clickTimerButton}
                             location={this.state.venue}
                         />
-                    </div>) : ""}
-                <Search getVenues={this.getVenues}></Search>
-                <div className="RestaurantsContainer">{this.state.venues.map(venue => {
-                    return (
-                        <div onClick={()=> {this.handleTimer(), this.state.venue=venue.venue.name, console.log(this.state.venue)}} className="RestaurantsList"  >
-                            <img className="placeholder" src={placeholder} />
-                            <div className="TextStyle">
-                                <div className="RestaurantStyle" key={venue.venue.name}>
-                                    {venue.venue.name}</div>
-                                <div className="LocationStyle">{venue.venue.location.address}</div>
+                    </div>
+                ) : (
+                    ""
+                )}
+                <Search getVenues={this.getVenues} />
+                <div className="RestaurantsContainer">
+                    {this.state.venues.map(venue => {
+                        return (
+                            <div
+                                onClick={() => {
+                                    this.handleTimer(),
+                                        (this.state.venue = venue.venue.name);
+                                    //console.log(this.state.venue);
+                                }}
+                                className="RestaurantsList"
+                            >
+                                <img
+                                    className="placeholder"
+                                    src={placeholder}
+                                />
+                                <div className="TextStyle">
+                                    <div
+                                        className="RestaurantStyle"
+                                        key={venue.venue.name}
+                                    >
+                                        {venue.venue.name}
+                                    </div>
+                                    <div className="LocationStyle">
+                                        {venue.venue.location.address}
+                                    </div>
+                                </div>
                             </div>
-                        </div>)
-                })}
+                        );
+                    })}
                 </div>
-                <div className="event__clock">
-                </div>
-
+                <div className="event__clock" />
             </div>
-
         );
     }
 }
