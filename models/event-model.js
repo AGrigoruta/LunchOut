148576800fbee905e78d65edb9346f1ab7826339
Onatek <ketano1711@gmail.com>
2019-04-20 16:58:29 +0300

    added date of creation to the event model

--- models/event-model.js
+++ models/event-model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+
 const eventSchema = new mongoose.Schema({
-    //expire_at: {type: Date, default: Date.now, expires: 60},
+    Date: { type:Date, default: Date.now},
     creatorID: String,
     location: String,
     startTime: String,
